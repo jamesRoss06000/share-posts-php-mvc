7e938f8952c7c2d62132013cbb0e179bbb227b7e
james ross <james_ross@outlook.fr>
2020-01-07 21:58:05 +0100

    test

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,13 @@
 <?php
-$user = getenv('USERNAME');
-$pass = getenv('PASSWORD');
-$data = getenv('DATABASE');
-
 // DB Params
-define('DB_HOST', 'remotemysql.com');
-define('DB_USER', $user);
-define('DB_PASS', $pass);
-define('DB_NAME', $data);
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'shareposts');
 // App root 
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://share-posts-php-mvc.herokuapp.com');
+define('URLROOT', '/php-share-posts');
 //Site name
 define('SITENAME', 'SharePosts');
 // App version
