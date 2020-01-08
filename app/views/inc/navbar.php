43b21c3932b5e982aa3bc21369911772720b163d
james ross <james_ross@outlook.fr>
2020-01-08 11:16:00 +0100

    composer and procfile added

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -15,7 +15,7 @@
           <a class="nav-link" href="<?php echo URLROOT; ?>">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo URLROOT; ?>/app/pages/about">About</a>
+          <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
         </li>
       </ul>
 
@@ -27,14 +27,14 @@
             <a class="nav-link" href="#">Welcome, <?php echo $_SESSION['user_name']; ?></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>/app/users/logout">Logout</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
           </li>
         <?php else : ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>/app/users/register">Register</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo URLROOT; ?>/app/users/login">Login</a>
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
           </li>
         <?php endif; ?>
       </ul>
